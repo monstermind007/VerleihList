ba3775885ff5003970d03d3adcc7cb4f9f49858b
SunnyNami <77606554+SunnyNami@users.noreply.github.com>
2021-01-26 12:23:29 +0100

    1.2

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form>
+    <div class="Inputfield">
+        <input type="email" name="email" required autocomplete="off">
+        <label>E-Mail</label>
+    </div>
+    <div class="Inputfield">
+        <input type="password" name="password" required autocomplete="off">
+        <label>Passwort</label>
+    </div>
+</form>
+</body>
+</html>
