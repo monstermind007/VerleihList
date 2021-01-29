7e8e1bce02b4073a1bddd97c32c4ec29bed6aad0
HWL <hwl.privat@gmail.com>
2021-01-29 10:09:16 +0100

    + SQL-Code test

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
 $mysql_server_name='localhost';
 $mysql_username='root';
 $mysql_password='123456';
@@ -11,17 +10,31 @@ if ($conn->connect_error) {die("ERROR: ".$conn->connect_error);}
 mysqli_query($conn, "set names 'utf8'");
 mysqli_select_db($mysql_database);
 
+
 // Info einschreiben
 $sql = "INSERT INTO TabelleName (Zeile1, Zeile2, Zeile3)
         VALUES ('Inhalt1', 'Inhalt2', 'Inhalt3')";
 
-$erfolg = mysqli_query($conn, $sql);
-if(!$erfolg) {die('Error: '.mysqli_error($conn));}
+$result = mysqli_query($conn, $sql);
+if(!$result) {die('Error: '.mysqli_error($conn));}
+
 
 // Info Lesen
 $sql = "SELECT Zeile1, Zeile2, Zeile3 FROM TabelleName";
 $ergibnis = $conn->query($sql);
 
-mysqli_close();
+if (!empty($passwort) || !empty($username)) {                    // Wenn Password oder Username ist nicht leer, dann
+    $sql = "SELECT Username, Password FROM account WHERE Username = '$username' AND Password = '$passwort'";
+    $result = mysqli_query($conn, $sql);
 
+    if (!$result) {
+        printf("Error:%s\n", mysqli_error($conn));
+        exit();
+    }
+    while ($row = mysqli_fetch_assoc($result)) {
+        $passwort_db = $row['Password'];
+        $username_db = $row['Username'];
+    }
+    mysqli_close();
+}
 ?>
