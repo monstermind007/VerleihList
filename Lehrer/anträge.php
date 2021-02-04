5d667c1b879eaff1e65e95808c7344ae9baef0bc
ElGurkus <l.d.walter@freenet.de>
2021-02-04 11:39:11 +0100

    +anträge.php für Lehrer existiert, kann noch nichts

---
+++ Lehrer/anträge.php
@@ -0,0 +1,118 @@
+<?php
+// Wenn nicht Angemeldet weiterleitung zur Hauptseite
+session_start();
+if (!isset($_SESSION['login'])) {
+    header('Location:../index.php');
+}
+if ("1" != $_SESSION["Lehrer"]){
+    header('Location:../index.php');
+}
+if (isset($_POST["logoff"])) {
+    session_destroy();
+    header('Location:../index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="../style.css"/>
+</head>
+<body id="body">
+<div class="container">
+    <nav class="navigation_oben">
+        <div class="nav_icon" onclick="toggleSidebar()">
+        </div>
+        <div class="navigation_oben__links">
+            <a class="active_link">Dashboard</a>
+            <a>Schüler</a>
+            <a>Materialliste</a>
+            <a>Profil</a>
+        </div>
+        <!--Rechte Navigationsleiste mit Notification Symbol-->
+        <div class="navigation_oben_rechts">
+            <a href="#">
+                <i class="notification" aria-hidden="true"></i>
+            </a>
+            <!--Profilbild Datenbank wenn möglich-->
+            </a>
+        </div>
+    </nav>
+
+    <!--Hauptteil -->
+    <main>
+        <div class="main_container">
+            <div class="main__title">
+                <h1>Hallo <?php echo $_SESSION["Vorname"], " ", $_SESSION["Name"]; ?> </h1>
+            </div>
+            <div class="Hauptteil">
+                <?php
+                @$dbconnection = mysqli_connect("134.255.218.71:3306", "materiallisteDB", "1McR2.71", "materialverleihDB");    //Die momentane verbindung
+                //@$dbconnection = mysqli_connect("localhost", "root", "", "materialverleihDB"); //Zum testen weil der Server weg ist
+                if (!$dbconnection) {
+                    error_log("Fehler beim Verbinden der Datenbank");
+                    die("Verbindungsfehler");
+                }
+                ?>
+            </div>
+        </div>
+    </main>
+    <!-- Seitliche Navigation Links -->
+    <div id="sidebar">
+        <div class="user">
+            <!-- Hier könnte man noch ein Profilbild einstllen-->
+            <h1><?php echo $_SESSION["Vorname"], " ", $_SESSION["Name"];?></h1>
+        </div>
+
+        <div class="sidebar_menu">
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="dashboard.php">Dashboard</a>
+            </div>
+            <h2>Schüler</h2>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="addstudent.php">Schüler Verwalten</a>
+            </div>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="addstudent.php">Schüler anlegen</a>
+            </div>
+            <h2>Materialliste</h2>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="Materialliste.php">Liste</a>
+            </div>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="addmaterials.php">Materialien anlegen</a>
+            </div>
+            <h2>Profil</h2>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="messages.php">Nachrichten</a>
+            </div>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="daten.php">Profildaten</a>
+            </div>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="profile.php">Daten ändern</a>
+            </div>
+            <div class="sidebar_link">
+                <i class="rechter_text"></i>
+                <a href="profile_password.php">Passwort ändern</a>
+            </div>
+            <div class="sidebar_logout">
+                <i class="rechter_text"></i>
+                <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+                    <input type="submit" value="Abmelden" name="logoff" id="logoff">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script>history.pushState({}, "", "")</script>
+</body>
+</html>
